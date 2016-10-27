758ae9c619f28b9d5847792db5e86af091b73a62
markusneuy <markus.neuy@publicispixelpark.de>
2016-10-27 22:07:23 +0200

    Initial commit.

--- dst/index.js
+++ dst/index.js
@@ -4,26 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _templateObject = _taggedTemplateLiteral(['test ', ' huhuhuh ', ''], ['test ', ' huhuhuh ', '']),
-    _templateObject2 = _taggedTemplateLiteral(['test'], ['test']),
-    _templateObject3 = _taggedTemplateLiteral(['test ', ''], ['test ', '']),
-    _templateObject4 = _taggedTemplateLiteral(['Test'], ['Test']);
-
 var _logtag = require('./lib/logtag');
 
 var _logtag2 = _interopRequireDefault(_logtag);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
 exports.default = _logtag2.default;
-
-
-var i = 5;
-console.log(_logtag2.default.error(_templateObject, new Error('Hallo'), new Error('Hallo 2')));
-console.log(_logtag2.default.done(_templateObject2));
-console.log(_logtag2.default.note(_templateObject3, { x: 1, y: 2 }));
-console.log(_logtag2.default.request(_templateObject3, i));
-console.log(_logtag2.default.warning(_templateObject2));
-console.log(_logtag2.default.critical(_templateObject4));
--- dst/lib/logtag.js
+++ dst/lib/logtag.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 var RED = '\x1b[31;1m';
 var GREEN = '\x1b[32;1m';
@@ -16,94 +16,94 @@ var END = '\x1b[0m';
  * This module exports helpers to create colored strings for logging.
  * This uses ANSI escape sequences.
  * @author Markus Neuy
- * @module LogTag
+ * @module logtag
  * @since 23.09.2016
  */
 exports.default = Object.create({
-    /**
-     * Turns a message into a console-log-string to indicate an error.
-     * The color will be red
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The error-message string
-     */
-    error: function error(strings) {
-        for (var _len = arguments.length, values = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-            values[_key - 1] = arguments[_key];
-        }
-
-        return RED + 'ERROR:    ' + concatTemplateString(strings, values) + ' ' + END;
-    },
-    /**
-     * Turns a message into a console-log-string to indicate a done.
-     * The color will be green
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The done-message string
-     */
-    done: function done(strings) {
-        for (var _len2 = arguments.length, values = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-            values[_key2 - 1] = arguments[_key2];
-        }
-
-        return GREEN + 'DONE:     ' + concatTemplateString(strings, values) + ' ' + END;
-    },
-    /**
-     * Turns a message into a console-log-string to indicate a note.
-     * The color will be magenta
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The note-message string
-     */
-    note: function note(strings) {
-        for (var _len3 = arguments.length, values = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
-            values[_key3 - 1] = arguments[_key3];
-        }
-
-        return BLUE + 'NOTE:     ' + concatTemplateString(strings, values) + ' ' + END;
-    },
-    /**
-     * Turns a message into a console-log-string to indicate a request.
-     * The color will be cyan
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The request-message string
-     */
-    request: function request(strings) {
-        for (var _len4 = arguments.length, values = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
-            values[_key4 - 1] = arguments[_key4];
-        }
-
-        return LIGHT_BLUE + 'REQUEST:  ' + concatTemplateString(strings, values) + ' ' + END;
-    },
-    /**
-     * Turns a message into a console-log-string to indicate a warning.
-     * The color will be yellow
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The warning-message string
-     */
-    warning: function warning(strings) {
-        for (var _len5 = arguments.length, values = Array(_len5 > 1 ? _len5 - 1 : 0), _key5 = 1; _key5 < _len5; _key5++) {
-            values[_key5 - 1] = arguments[_key5];
-        }
-
-        return ORANGE + 'WARNING:  ' + concatTemplateString(strings, values) + ' ' + END;
-    },
-    /**
-     * Turns a message into a console-log-string to indicate something critical.
-     * The color will be pink
-     * @param {Array} strings Template base strings
-     * @param {...*} values Values of the template
-     * @returns {String} The critical-message string
-     */
-    critical: function critical(strings) {
-        for (var _len6 = arguments.length, values = Array(_len6 > 1 ? _len6 - 1 : 0), _key6 = 1; _key6 < _len6; _key6++) {
-            values[_key6 - 1] = arguments[_key6];
-        }
-
-        return PINK + 'CRITICAL: ' + concatTemplateString(strings, values) + ' ' + END;
+  /**
+   * Turns a message into a console-log-string to indicate an error.
+   * The color will be red
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The error-message string
+   */
+  error: function error(strings) {
+    for (var _len = arguments.length, values = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      values[_key - 1] = arguments[_key];
     }
+
+    return RED + 'ERROR:    ' + concatTemplateString(strings, values) + ' ' + END;
+  },
+  /**
+   * Turns a message into a console-log-string to indicate a done.
+   * The color will be green
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The done-message string
+   */
+  done: function done(strings) {
+    for (var _len2 = arguments.length, values = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+      values[_key2 - 1] = arguments[_key2];
+    }
+
+    return GREEN + 'DONE:     ' + concatTemplateString(strings, values) + ' ' + END;
+  },
+  /**
+   * Turns a message into a console-log-string to indicate a note.
+   * The color will be magenta
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The note-message string
+   */
+  note: function note(strings) {
+    for (var _len3 = arguments.length, values = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
+      values[_key3 - 1] = arguments[_key3];
+    }
+
+    return BLUE + 'NOTE:     ' + concatTemplateString(strings, values) + ' ' + END;
+  },
+  /**
+   * Turns a message into a console-log-string to indicate a request.
+   * The color will be cyan
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The request-message string
+   */
+  request: function request(strings) {
+    for (var _len4 = arguments.length, values = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
+      values[_key4 - 1] = arguments[_key4];
+    }
+
+    return LIGHT_BLUE + 'REQUEST:  ' + concatTemplateString(strings, values) + ' ' + END;
+  },
+  /**
+   * Turns a message into a console-log-string to indicate a warning.
+   * The color will be yellow
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The warning-message string
+   */
+  warning: function warning(strings) {
+    for (var _len5 = arguments.length, values = Array(_len5 > 1 ? _len5 - 1 : 0), _key5 = 1; _key5 < _len5; _key5++) {
+      values[_key5 - 1] = arguments[_key5];
+    }
+
+    return ORANGE + 'WARNING:  ' + concatTemplateString(strings, values) + ' ' + END;
+  },
+  /**
+   * Turns a message into a console-log-string to indicate something critical.
+   * The color will be pink
+   * @param {Array} strings Template base strings
+   * @param {...*} values Values of the template
+   * @returns {String} The critical-message string
+   */
+  critical: function critical(strings) {
+    for (var _len6 = arguments.length, values = Array(_len6 > 1 ? _len6 - 1 : 0), _key6 = 1; _key6 < _len6; _key6++) {
+      values[_key6 - 1] = arguments[_key6];
+    }
+
+    return PINK + 'CRITICAL: ' + concatTemplateString(strings, values) + ' ' + END;
+  }
 });
 
 /**
@@ -116,21 +116,11 @@ exports.default = Object.create({
  */
 
 function concatTemplateString(strings, values) {
-    //let traces = '';
-
-    var traces = values.map(function (val) {
-        return val instanceof Error ? '\n' + val.stack : '';
-    }).join('');
-    var result = strings.reduce(function (prev, curr, i) {
-        return '' + prev + values[i - 1].toString() + curr;
-    });
-    return '' + result + traces;
-
-    /*return (strings.reduce((prev, curr, i) => {
-        const val = values[i-1];
-        if (val instanceof Error) {
-            traces += `\n${val.stack}`;
-        }
-        return `${prev}${val.toString()}${curr}`;
-    })) + traces;*/
+  var traces = values.map(function (val) {
+    return val instanceof Error ? '\n' + val.stack : '';
+  }).join('');
+  var result = strings.reduce(function (prev, curr, i) {
+    return '' + prev + values[i - 1].toString() + curr;
+  });
+  return '' + result + traces;
 }
